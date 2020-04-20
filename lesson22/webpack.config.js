c1ad81068df9f08422f3f8e4b8ec95d07b4a5e05
juncid <juncid@gmail.com>
2020-04-20 19:14:31 -0400

    Lesson30: Extract css using webpack and vue-loader.

--- lesson22/webpack.config.js
+++ lesson22/webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './src/main.js',
@@ -41,11 +42,11 @@ module.exports = {
             // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': [
-              'vue-style-loader',
-              'css-loader',
-              'sass-loader'
-            ],
+
+            'scss': ExtractTextPlugin.extract({
+              use: 'css-loader!sass-loader',
+              fallback: 'vue-style-loader'
+            }),
             'sass': [
               'vue-style-loader',
               'css-loader',
@@ -69,6 +70,9 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin("style.css"),
+  ],
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
